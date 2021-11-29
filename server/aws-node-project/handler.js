7b869b33a6812ea8d58d09cd014cf2cf38f7813c
chandrashekar112 <chandrashekar.sukkali@ness.com>
2021-11-29 17:16:20 +0530

    'changefile

--- server/aws-node-project/handler.js
+++
@@ -1,8 +0,0 @@
-const awsServerlessExpress = require("aws-serverless-express"); /* eslint-disable no-trailing-spaces */ 
-const app = require('./src/index');
-
-const server = awsServerlessExpress.createServer(app);
-
-exports.handler = (event, context) => {
-  return awsServerlessExpress.proxy(server, event, context);
-}
--- server/aws-node-project/src/routes/indexRouter.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-const routes = express.Router({ mergeParams: true });
-var AWS = require("aws-sdk");
-
-routes.post("/", async (req, res) => {});
-
-module.exports = routes;
